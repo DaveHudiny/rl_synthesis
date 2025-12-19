added model checking eval for shields; rounding precision for correct calls WIP

rl_src/shielding/shield_processor.py
@@ -46,17 +46,19 @@ class ShieldProcessor:
         # model checking results for debugging
         if debug:
             reach_formula = stormpy.parse_properties("Pmax=? [ F \"goal\" ]")
-            reward_formula = stormpy.parse_properties("Rmax=? [ F \"goal\" ]")
+            reward_formula = stormpy.parse_properties("Rmax=? [ C<=50 ]")
             until_formula = stormpy.parse_properties("Pmax=? [ !\"bad\" U \"goal\" ]")
+            goal_formula = stormpy.parse_properties("Pmax=? [ F \"goal\" ]")
             reach_result = stormpy.model_checking(mdp, reach_formula[0])
             reward_result = stormpy.model_checking(mdp, reward_formula[0])
             until_result = stormpy.model_checking(mdp, until_formula[0])
+            goal_result = stormpy.model_checking(mdp, goal_formula[0])
             print("Max reachability probabilities to goal from initial state:", reach_result.get_values()[mdp.initial_states[0]])
             print("Max expected rewards to goal from initial state:", reward_result.get_values()[mdp.initial_states[0]])
             print("Max until probabilities to goal from initial state:", until_result.get_values()[mdp.initial_states[0]])
-            print(vmin)
+            # print(vmin)
+            # print(self.bad_states)
             exit()
-
 
         observation_to_state = [None] * model.nr_observations
         for state in range(model.nr_states):
@@ -86,6 +88,8 @@ class ShieldProcessor:
         else:
             raise ValueError(f"Unknown shield type: {shield_type}")
         
+        self.shield.rounding_precision = 6
+        
         if self.shield_folder is not None:
             assert type(self.shield) in [rl_src.shielding.shields.SelfConstructingShieldConstructionSafe, rl_src.shielding.shields.SelfConstructingShieldConstructionUnsafe], "Saving shield can only be used with self-constructing shields."
         
@@ -98,6 +102,7 @@ class ShieldProcessor:
             original_model_nr_states=self.shield.model_info.model.nr_states,
             observation_to_state=self.shield.model_info.observation_to_state,
             memory=self.shield.memory,
+            rounding_precision=self.shield.rounding_precision,
             initial_node=self.shield.initial_node,
             current_action_distributions=self.shield.current_action_distributions
         )
@@ -117,6 +122,7 @@ class ShieldProcessor:
         assert self.shield.model_info.model.nr_states == shield_data.original_model_nr_states
         assert self.shield.model_info.observation_to_state == shield_data.observation_to_state
         assert self.shield.memory == shield_data.memory
+        assert self.shield.rounding_precision == shield_data.rounding_precision
 
         self.shield.initial_node = shield_data.initial_node
         self.shield.current_action_distributions = shield_data.current_action_distributions
@@ -129,6 +135,19 @@ class ShieldProcessor:
         else:
             uniform_prob = 1.0 / len(distribution)
             return [uniform_prob for _ in distribution]
+        
+    def map_played_distribution(self, played_probs, current_state):
+        current_state_choice_labels = []
+
+        for choice in range(self.shield.model_info.model.transition_matrix.get_row_group_start(current_state), self.shield.model_info.model.transition_matrix.get_row_group_end(current_state)):
+            current_state_choice_labels.append(self.shield.model_info.model.choice_labeling.get_labels_of_choice(choice).pop())
+
+        mapped_played_distribution = [played_probs[self.actions.index(action)] for action in current_state_choice_labels]
+        mapped_played_distribution = self.fix_distribution(mapped_played_distribution)
+
+        mapped_played_distribution = [round(prob, self.shield.rounding_precision) for prob in mapped_played_distribution]
+
+        return mapped_played_distribution, current_state_choice_labels
 
     def compute_new_logits(self, valuations : list, integers : list, prev_actions : list, played_logits : tf.Tensor, resets : list) -> tf.Tensor:
         """ A dummy shielding method that always allows the action.
@@ -147,20 +166,14 @@ class ShieldProcessor:
         for i in range(len(valuations)):
 
             current_state = self.shield.model_info.observation_to_state[integers[i][0]]
-            current_state_choice_labels = []
-
-            for choice in range(self.shield.model_info.model.transition_matrix.get_row_group_start(current_state), self.shield.model_info.model.transition_matrix.get_row_group_end(current_state)):
-                current_state_choice_labels.append(self.shield.model_info.model.choice_labeling.get_labels_of_choice(choice).pop())
-
-            mapped_played_distribution = [played_probs[i][self.actions.index(action)] for action in current_state_choice_labels]
-            mapped_played_distribution = self.fix_distribution(mapped_played_distribution)
+            mapped_played_distribution, current_state_choice_labels = self.map_played_distribution(played_probs[i], current_state)
 
             distribution = self.shield.correct(prev_actions[i], current_state, mapped_played_distribution, resets[i], i)
 
             distribution = [distribution[current_state_choice_labels.index(action)] if action in current_state_choice_labels else 0.0 for action in self.actions]
 
             distributions.append(distribution)
-        
+
         distributions = np.array(distributions, dtype=np.float_)
         # Convert the boolean mask to logits.
         masked_logits = tf.math.log(distributions + 1e-10)

rl_src/shielding/shields.py
@@ -30,6 +30,8 @@ class Shield:
         self.added_nonoptimal_actions = 0
         self.trace_count = 0
 
+        self.rounding_precision = 6
+
     def reset_stats(self):
         self.shield_calls = 0
         self.blocked_actions = 0
@@ -267,12 +269,43 @@ class Node:
     distributions: list[list[float]]
     state_index: int
     value: float
+    node_index: int
 
     def number_of_tree_nodes(self) -> int:
         count = 1
         for succ in self.successors.values():
             count += succ.number_of_tree_nodes()
         return count
+
+    def get_node(self, id) -> "Node":
+        if self.node_index == id:
+            return self
+        for succ in self.successors.values():
+            node = succ.get_node(id)
+            if node is not None:
+                return node
+        return None
+    
+    def get_index_to_node_map(self, index_map={}) -> dict[int, "Node"]:
+        index_map[self.node_index] = self
+        for succ in self.successors.values():
+            succ.get_index_to_node_map(index_map)
+        return index_map
+    
+    def get_state_indices_map(self, index_map={}) -> dict[int, int]:
+        index_map[self.node_index] = self.state_index
+        for succ in self.successors.values():
+            succ.get_state_indices_map(index_map)
+        return index_map
+
+    def get_node_to_successor_index_to_state_map(self, node_map={}) -> dict[int, dict[tuple[int, int], int]]:
+        succ_map = {}
+        for (state, distribution_index), succ in self.successors.items():
+            succ_map[(state, distribution_index)] = succ.node_index
+        node_map[self.node_index] = succ_map
+        for succ in self.successors.values():
+            succ.get_node_to_successor_index_to_state_map(node_map)
+        return node_map
     
 
 class SelfConstructingShield(Shield):
@@ -286,7 +319,8 @@ class SelfConstructingShield(Shield):
         initial_state = model_info.model.initial_states[0]
 
         # get vmin
-        self.initial_node = Node({}, None, None, [], initial_state, self.model_info.vmin[initial_state])
+        self.initial_node = Node({}, None, None, [], initial_state, self.model_info.vmin[initial_state], 0)
+        self.num_nodes = 1
 
         self.current_nodes = [self.initial_node]
 
@@ -353,16 +387,21 @@ class SelfConstructingShield(Shield):
             self.vmin_actions_distributions.append(vmin_actions_distributions)
             self.vmin_actions.append(vmin_actions)
 
-    def point_in_convex_hull(self, hull_vertices, point, tolerance=1e-12):
+    def point_in_convex_hull(self, hull_vertices, point, tolerance=1e-6):
+        if point in hull_vertices:
+            self.convex_point_max_index = hull_vertices.index(point)
+            return True
         self.convex_point_max_index = None
         vertices = np.array([tuple(v) for v in hull_vertices])
+        point = [val if val >= tolerance else 0.0 for val in point]
         p = np.array(point)
         # Use coordinates directly to check if point is in the convex hull (i.e. the point is linear combination of the basis vectors with non-negative coefficients summing to 1)
         c = np.zeros(len(vertices))
         A_eq = np.vstack([vertices.T, np.ones(len(vertices))])
-        b_eq = np.append(p, 1)
+        b_eq = np.append(p / np.sum(p), 1)
         bounds = [(0, 1)] * len(vertices)
         res = linprog(c, A_eq=A_eq, b_eq=b_eq, bounds=bounds, method='highs')
+        # print(res.x, res.success, res.status, res.message)
         # Check if the solution coefficients are all within bounds and sum to 1
         if not (res.success and res.status == 0):
             return False
@@ -400,6 +439,34 @@ class SelfConstructingShield(Shield):
                     best_value = q_value
             node.value = best_value
             node = node.predecessor
+
+    def correct_for_given_node(self, last_action, current_state, distribution, reset, node):
+
+        # TODO handle memory
+
+        output_distribution = distribution
+
+        if node is None:
+            all_allowed_ditributions = self.vmin_actions_distributions[current_state]
+        else:
+            all_allowed_ditributions = self.vmin_actions_distributions[current_state] + node.distributions
+
+        if not self.point_in_convex_hull(all_allowed_ditributions, distribution):
+            output_distribution = clamp_distribution(distribution, self.vmin_actions[current_state])
+            if self.static_shield_clamp_to_existing_distributions and output_distribution not in all_allowed_ditributions:
+                self.point_in_convex_hull(all_allowed_ditributions, output_distribution) # to get the max index
+                output_distribution = all_allowed_ditributions[self.convex_point_max_index]
+        elif self.static_shield_clamp_to_existing_distributions and output_distribution not in all_allowed_ditributions:
+            output_distribution = all_allowed_ditributions[self.convex_point_max_index]
+
+        if output_distribution in all_allowed_ditributions:
+            last_distribution_index = all_allowed_ditributions.index(output_distribution)
+        else:
+            assert not self.static_shield_clamp_to_existing_distributions, "This should not happen, as the distribution was clamped to allowed distributions."
+            last_distribution_index = len(all_allowed_ditributions)
+
+        return output_distribution, last_distribution_index
+
 
     def correct(self, last_action, current_state, distribution, reset, trace_index=0):
         self.shield_calls += 1
@@ -495,7 +562,8 @@ class SelfConstructingShieldConstructionUnsafe(SelfConstructingShield):
                 if (current_state, self.last_distribution_indices[trace_index]) not in self.current_nodes[trace_index].successors.keys():
                     all_dist = self.vmin_actions_distributions[self.current_nodes[trace_index].state_index] + self.current_nodes[trace_index].distributions
                     assert self.last_distribution_indices[trace_index] is None and len(all_dist) == 0 or self.last_distribution_indices[trace_index] < len(all_dist)
-                    self.current_nodes[trace_index].successors[(current_state, self.last_distribution_indices[trace_index])] = Node({}, self.current_nodes[trace_index], self.last_distribution_indices[trace_index], [], current_state, self.model_info.vmin[current_state])
+                    self.current_nodes[trace_index].successors[(current_state, self.last_distribution_indices[trace_index])] = Node({}, self.current_nodes[trace_index], self.last_distribution_indices[trace_index], [], current_state, self.model_info.vmin[current_state], self.num_nodes)
+                    self.num_nodes += 1
                     self.current_nodes[trace_index] = self.current_nodes[trace_index].successors[(current_state, self.last_distribution_indices[trace_index])]
                 else:
                     self.current_nodes[trace_index] = self.current_nodes[trace_index].successors[(current_state, self.last_distribution_indices[trace_index])]
@@ -619,7 +687,8 @@ class SelfConstructingShieldConstructionSafe(SelfConstructingShield):
                 if (current_state, self.last_distribution_indices[trace_index]) not in self.current_nodes[trace_index].successors.keys():
                     all_dist = self.vmin_actions_distributions[self.current_nodes[trace_index].state_index] + self.current_nodes[trace_index].distributions
                     assert self.last_distribution_indices[trace_index] is None and len(all_dist) == 0 or self.last_distribution_indices[trace_index] < len(all_dist)
-                    self.current_nodes[trace_index].successors[(current_state, self.last_distribution_indices[trace_index])] = Node({}, self.current_nodes[trace_index], self.last_distribution_indices[trace_index], [], current_state, self.model_info.vmin[current_state])
+                    self.current_nodes[trace_index].successors[(current_state, self.last_distribution_indices[trace_index])] = Node({}, self.current_nodes[trace_index], self.last_distribution_indices[trace_index], [], current_state, self.model_info.vmin[current_state], self.num_nodes)
+                    self.num_nodes += 1
                     self.current_nodes[trace_index] = self.current_nodes[trace_index].successors[(current_state, self.last_distribution_indices[trace_index])]
                 else:
                     self.current_nodes[trace_index] = self.current_nodes[trace_index].successors[(current_state, self.last_distribution_indices[trace_index])]

rl_src/tools/memoryless_fsc_extraction.py
@@ -3,7 +3,13 @@ from rl_src.environment.environment_wrapper_vec import EnvironmentWrapperVec
 from tf_agents.policies.tf_policy import TFPolicy
 import tensorflow as tf
 
-def extract_memory_less_fsc_actions(environment : EnvironmentWrapperVec, policy: TFPolicy, get_probs : bool = False):
+def extract_memory_less_fsc_actions(environment : EnvironmentWrapperVec, policy: TFPolicy, get_probs : bool = False, compile_policy : bool = False):
+
+    if compile_policy:
+        policy_function = tf.function(policy.distribution)
+    else:
+        policy_function = policy.distribution
+
     nr_obs = environment.vectorized_simulator.simulator.observation_by_ids.shape[0]
     fake_observations = environment.create_fake_timestep_from_observation_integer(range(nr_obs))
     initial_state = policy.get_initial_state(batch_size=nr_obs)
@@ -11,8 +17,11 @@ def extract_memory_less_fsc_actions(environment : EnvironmentWrapperVec, policy:
         action_step = policy.action(fake_observations, initial_state)
         actions = action_step.action.numpy()
     else:
-        action_step = policy.distribution(fake_observations, initial_state)
+        action_step = policy_function(fake_observations, initial_state)
         logits = action_step.action.logits
-        actions = tf.nn.softmax(logits, axis=-1).numpy()
-    return actions
+        actions = tf.nn.softmax(logits).numpy()
+    obs_list = fake_observations.observation['observation'].numpy().tolist()
+    obs_tuples = [tuple(obs) for obs in obs_list]
+    observation_to_action = dict(zip(obs_tuples, actions.tolist()))
+    return actions, observation_to_action
     

shielding.py
@@ -25,6 +25,9 @@ from tf_agents.policies.tf_policy import TFPolicy
 from rl_src.tools.evaluation_results_class import EvaluationResults
 from tf_agents.trajectories import Trajectory
 
+from rl_src.tools.memoryless_fsc_extraction import extract_memory_less_fsc_actions
+from rl_src.shielding.shielded_model_checking import model_check_given_policy_and_shield
+
 from tqdm import tqdm
 
 # PAYNT implementation imports
@@ -106,14 +109,16 @@ def custom_loop(policy : TFPolicy, environment : EnvironmentWrapperVec, shield_p
 
         # logits = tf.math.log(probs)
         # End of an identity block.
-
-        logits = shield_processor.compute_new_logits(
-            valuations=time_step.observation["observation"].numpy().tolist(),
-            integers=time_step.observation["integer"].numpy().tolist(),
-            prev_actions=prev_actions.numpy().tolist(),
-            played_logits=distribution.logits,
-            resets=time_step.is_first().numpy().tolist()
-        )
+        if shield_processor is not None:
+            logits = shield_processor.compute_new_logits(
+                valuations=time_step.observation["observation"].numpy().tolist(),
+                integers=time_step.observation["integer"].numpy().tolist(),
+                prev_actions=prev_actions.numpy().tolist(),
+                played_logits=distribution.logits,
+                resets=time_step.is_first().numpy().tolist()
+            )
+        else:
+            logits = distribution.logits
 
         action = tf.random.categorical(logits, 1, dtype=tf.int32, seed=seed)
         prev_actions = action
@@ -155,8 +160,9 @@ def set_global_seeds(seed):
 @click.option("--load-shield", type=str, default=None, help="Path to load a pre-trained shield from.")
 @click.option("--uniform-random-policy", is_flag=True, default=False, help="Whether to use a uniform random policy for evaluation instead of a trained agent.")
 @click.option("--eval-file", type=str, default=None, help="File to save evaluation results.")
+@click.option("--model-checking-eval", is_flag=True, default=False, help="Whether to perform model checking based evaluation.")
 @click.option("--seed", type=int, default=None, help="Random seed for reproducibility.")
-def main(project, nu, shield, load_agent, save_agent, agent_training, shield_memory, training_iterations, episode_length, num_environments, num_parallel_environments, model_debug, save_shield, load_shield, uniform_random_policy, eval_file, seed):
+def main(project, nu, shield, load_agent, save_agent, agent_training, shield_memory, training_iterations, episode_length, num_environments, num_parallel_environments, model_debug, save_shield, load_shield, uniform_random_policy, eval_file, model_checking_eval, seed):
     project_path = project
     project_name = os.path.basename(os.path.normpath(project_path))
     prism_path = os.path.join(project_path, "sketch.templ")
@@ -232,23 +238,44 @@ def main(project, nu, shield, load_agent, save_agent, agent_training, shield_mem
         policy.set_greedy(False)
         policy.set_policy_masker()
         policy.set_return_real_logits(True)
+        # _, obs_to_action = extract_memory_less_fsc_actions(environment, policy, get_probs = True, compile_policy=True)
+        # from rl_src.shielding.custom_policy import create_custom_policy
+        # policy = create_custom_policy(environment, obs_to_action)
         compile_policy = True
 
     # Custom simulation loop for evaluation
-    trajectory_buffer = TrajectoryBuffer(environment)
-    evaluation_result = EvaluationResults()
-    num_eval_iterations = math.ceil(num_environments / num_parallel_environments)
-    environment.temporarily_set_num_envs(num_parallel_environments)
-    start_time = time.time()
-    for eval_iteration in tqdm(range(num_eval_iterations)):
-        custom_loop(policy, environment, shield_processor, num_parallel_environments, episode_length, trajectory_buffer, compile_policy=compile_policy, seed=seed)
-        if save_shield is not None and shield_processor is not None:
-            shield_processor.save_shield(shield_processor.shield_folder, iteration=eval_iteration)
-    eval_elapsed_time = time.time() - start_time
-    environment.reset_num_envs() # Sets the number of environments back to original value.
-    print(f"Evaluation loop took {eval_elapsed_time:.2f} seconds.")
-    trajectory_buffer.final_update_of_results(evaluation_result.update)
-    evaluation_result.log_evaluation_info()
+    if model_checking_eval:
+        assert shield_processor is not None, "Shield processor must be provided for model checking evaluation."
+        actions, _ = extract_memory_less_fsc_actions(environment, policy, get_probs = True, compile_policy=compile_policy)
+
+        mapped_actions = []
+        state_choice_labels= []
+        for state in range(shield_processor.shield.model_info.model.nr_states):
+            obs = shield_processor.shield.model_info.observation_to_state.index(state)
+            mapped_distribution, choice_labels = shield_processor.map_played_distribution(actions[obs], state)
+            mapped_actions.append(mapped_distribution)
+            state_choice_labels.append(choice_labels)
+
+        model_check_given_policy_and_shield(mapped_actions, shield_processor.shield, episode_length=episode_length)        
+
+
+
+        exit()
+    else:
+        trajectory_buffer = TrajectoryBuffer(environment)
+        evaluation_result = EvaluationResults()
+        num_eval_iterations = math.ceil(num_environments / num_parallel_environments)
+        environment.temporarily_set_num_envs(num_parallel_environments)
+        start_time = time.time()
+        for eval_iteration in tqdm(range(num_eval_iterations)):
+            custom_loop(policy, environment, shield_processor, num_parallel_environments, episode_length, trajectory_buffer, compile_policy=compile_policy, seed=seed)
+            if save_shield is not None and shield_processor is not None:
+                shield_processor.save_shield(shield_processor.shield_folder, iteration=eval_iteration)
+        eval_elapsed_time = time.time() - start_time
+        environment.reset_num_envs() # Sets the number of environments back to original value.
+        print(f"Evaluation loop took {eval_elapsed_time:.2f} seconds.")
+        trajectory_buffer.final_update_of_results(evaluation_result.update)
+        evaluation_result.log_evaluation_info()
 
     # Simulator
     # evaluation_result = evaluate_policy_in_model(policy, args, environment, tf_env, max_steps=episode_length, shield_processor=shield_processor)