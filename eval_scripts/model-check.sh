a6088b8ec1a92a99160d2810a8cac56440980063
Filip Macak <filip.macak1999@gmail.com>
2025-12-30 12:40:22 +0100

    added model checking eval script

---
+++ eval_scripts/model-check.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+SUBFOLDER="$1"
+if [ -z "$SUBFOLDER" ]; then
+	echo "Usage: $0 <results_subfolder>"
+	exit 1
+fi
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/dpm  --episode-length 100 --num-environments 1024 --num-parallel-environments 256 --agent greedy-iter-1000 --nu 0.05 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/dpm  --episode-length 100 --num-environments 1024 --num-parallel-environments 256 --agent greedy-iter-1000 --nu 0.2 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/dpm  --episode-length 100 --num-environments 1024 --num-parallel-environments 256 --agent greedy-iter-1000 --nu 0.01 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/dpm  --episode-length 100 --num-environments 1024 --num-parallel-environments 256 --agent safe-iter-1000 --nu 0.05 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/dpm  --episode-length 100 --num-environments 1024 --num-parallel-environments 256 --agent safe-iter-1000 --nu 0.2 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"//model-check-results.csv --model-path models/shielding_test/dpm  --episode-length 100 --num-environments 1024 --num-parallel-environments 256 --agent safe-iter-1000 --nu 0.01 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"//model-check-results.csv --model-path models/shielding_test/dpm  --episode-length 100 --num-environments 1024 --num-parallel-environments 256 --agent uniform-iter-1000 --nu 0.05 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --uniform-random-agent --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/dpm  --episode-length 100 --num-environments 1024 --num-parallel-environments 256 --agent uniform-iter-1000 --nu 0.2 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --uniform-random-agent --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/dpm  --episode-length 100 --num-environments 1024 --num-parallel-environments 256 --agent uniform-iter-1000 --nu 0.01 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --uniform-random-agent --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/slippy-drone  --episode-length 100 --num-environments 1024 --num-parallel-environments 256 --agent greedy-iter-4000 --nu 0.05 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/slippy-drone  --episode-length 100 --num-environments 1024 --num-parallel-environments 256 --agent greedy-iter-4000 --nu 0.2 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/slippy-drone  --episode-length 100 --num-environments 1024 --num-parallel-environments 256 --agent greedy-iter-4000 --nu 0.01 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/slippy-drone  --episode-length 100 --num-environments 1024 --num-parallel-environments 256 --agent safe-iter-1000 --nu 0.05 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/slippy-drone  --episode-length 100 --num-environments 1024 --num-parallel-environments 256 --agent safe-iter-1000 --nu 0.2 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/slippy-drone  --episode-length 100 --num-environments 1024 --num-parallel-environments 256 --agent safe-iter-1000 --nu 0.01 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/slippy-drone  --episode-length 100 --num-environments 1024 --num-parallel-environments 256 --agent uniform-iter-1000 --nu 0.05 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --uniform-random-agent --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/slippy-drone  --episode-length 100 --num-environments 1024 --num-parallel-environments 256 --agent uniform-iter-1000 --nu 0.2 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --uniform-random-agent --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/slippy-drone  --episode-length 100 --num-environments 1024 --num-parallel-environments 256 --agent uniform-iter-1000 --nu 0.01 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --uniform-random-agent --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/test-corridor  --episode-length 50 --num-environments 1024 --num-parallel-environments 256 --agent greedy-iter-100 --nu 0.1 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/test-corridor  --episode-length 50 --num-environments 1024 --num-parallel-environments 256 --agent greedy-iter-100 --nu 0.2 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/test-corridor  --episode-length 50 --num-environments 1024 --num-parallel-environments 256 --agent greedy-iter-100 --nu 0.05 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/test-corridor  --episode-length 50 --num-environments 1024 --num-parallel-environments 256 --agent safe-iter-4000 --nu 0.1 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/test-corridor  --episode-length 50 --num-environments 1024 --num-parallel-environments 256 --agent safe-iter-4000 --nu 0.2 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/test-corridor  --episode-length 50 --num-environments 1024 --num-parallel-environments 256 --agent safe-iter-4000 --nu 0.05 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/test-corridor  --episode-length 50 --num-environments 1024 --num-parallel-environments 256 --agent uniform-iter-100 --nu 0.1 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --uniform-random-agent --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/test-corridor  --episode-length 50 --num-environments 1024 --num-parallel-environments 256 --agent uniform-iter-100 --nu 0.2 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --uniform-random-agent --model-checking-eval
+
+python3 run_benchmark.py --results-file results/"$SUBFOLDER"/model-check-results.csv --model-path models/shielding_test/test-corridor  --episode-length 50 --num-environments 1024 --num-parallel-environments 256 --agent uniform-iter-100 --nu 0.05 --number-of-evaluations 1 --log-file results/"$SUBFOLDER"/model-check-results.log --uniform-random-agent --model-checking-eval
