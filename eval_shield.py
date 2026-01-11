59aa75caa7448830ed872b3f1854880ace6425d9
Filip Macak <filip.macak1999@gmail.com>
2026-01-11 12:54:27 +0100

    fixed eval script

--- eval_shield.py
+++ eval_shield.py
@@ -198,8 +198,10 @@ def main(results_folder, shield, shield_memory, shield_path, number_of_evaluatio
 
             avg_risk = total_risk / number_of_evaluations
             avg_reward = total_reward / number_of_evaluations
+            avg_shield_calls = total_shield_calls // number_of_evaluations
+            avg_blocked_actions = total_blocked_actions // number_of_evaluations
 
-            add_result_to_csv(main_csv_path, model, agent, nu, shield, shield_name, avg_risk, avg_reward)
+            add_result_to_csv(main_csv_path, df, model, agent, nu, shield, shield_name, avg_risk, avg_reward, avg_shield_calls, avg_blocked_actions)
 
 
 
