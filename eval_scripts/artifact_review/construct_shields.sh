8b585ccd657dda70232d04e0494975a8401552c2
Filip Macak <filip.macak1999@gmail.com>
2026-04-27 09:42:36 +0200

    updating scripts

--- eval_scripts/artifact_review/construct_shields.sh
+++ eval_scripts/artifact_review/construct_shields.sh
@@ -1,8 +1,10 @@
-python3 construct_shield_for_eval.py --results-folder results/shield-construction-review --shield-model dpm --artifact-review&
-
 python3 construct_shield_for_eval.py --results-folder results/shield-construction-review --shield-model corridor --artifact-review&
 
 python3 construct_shield_for_eval.py --results-folder results/shield-construction-review --shield-model drone --artifact-review&
+
+wait
+
+python3 construct_shield_for_eval.py --results-folder results/shield-construction-review --shield-model dpm --artifact-review&
 
 python3 construct_shield_for_eval.py --results-folder results/shield-construction-review --shield-model drone-b&
 
